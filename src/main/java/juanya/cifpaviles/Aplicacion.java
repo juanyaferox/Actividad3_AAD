84475c516ee837c00c7b163422102a5174dcac56
juanya <juaniago2001@gmail.com>
2024-01-17 04:00:24 +0100

    main

--- src/main/java/juanya/cifpaviles/Aplicacion.java
+++ src/main/java/juanya/cifpaviles/Aplicacion.java
@@ -7,10 +7,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Aplicacion {
 
-    @Bean
-    public Main applicationStartupRunner() {
-        return new Main();
-    }
     public static void main(String[] args) {
         SpringApplication.run(Aplicacion.class, args);
     }
