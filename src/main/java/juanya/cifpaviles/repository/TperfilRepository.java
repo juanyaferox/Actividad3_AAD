628510cd794561aaeefd8b05d773cfa020b2bda3
juanya <juaniago2001@gmail.com>
2024-01-18 00:09:02 +0100

    va como va

--- src/main/java/juanya/cifpaviles/repository/TperfilRepository.java
+++ src/main/java/juanya/cifpaviles/repository/TperfilRepository.java
@@ -7,4 +7,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TperfilRepository extends JpaRepository<Tperfil, String> {
 
+    default void insertarTperfil(String pkidUsuario, String cpassword, String fkIdPeregrino) {
+            Tperfil nuevoTperfil = new Tperfil();
+            nuevoTperfil.setPkidUsuario(pkidUsuario);
+            nuevoTperfil.setCpassword(cpassword);
+            nuevoTperfil.setFkidPeregrino(fkIdPeregrino);
+            save(nuevoTperfil);
+    }
 }
